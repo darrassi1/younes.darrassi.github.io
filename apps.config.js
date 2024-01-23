480a285dcbfe5a96fe8484a596f6d0e34bdfa89e
darrassi1 <105133429+darrassi1@users.noreply.github.com>
2024-01-23 07:05:19 +0100

    Update apps.config.js

--- apps.config.js
+++ apps.config.js
@@ -29,7 +29,7 @@ const apps = [
     },
     {
         id: "about-vivek",
-        title: "About Vivek",
+        title: "About Younes Darrassi",
         icon: './themes/Yaru/system/user-home.png',
         disabled: false,
         favourite: true,
